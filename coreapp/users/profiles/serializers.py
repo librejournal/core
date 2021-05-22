b772d2be970b1c324f0b070f4c120fcbcf9cecd3
berkkarahan <berkkarahan00@gmail.com>
2021-05-22 16:37:55 +0300

    update follow unfollow serializer

--- coreapp/users/profiles/serializers.py
+++ coreapp/users/profiles/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 from coreapp.users.models import Profile, PROFILE_TYPE_CHOICES
 from coreapp.users.serializers import UserSerializer, TinyUserSerializer
-from coreapp.stories.models import StoryLocations
+from coreapp.stories.models import StoryLocations, StoryTags
 from coreapp.stories.serializers import StoryLocationSerializer
 
 User = get_user_model()
@@ -41,33 +41,55 @@ class ProfileSerializer(serializers.ModelSerializer):
 
 
 class FollowUnfollowSerializer(serializers.Serializer):
-    profile_uuid_list = serializers.ListField(child=serializers.UUIDField())
-    story_location_uuid_list = serializers.ListField(child=serializers.UUIDField())
+    profile_id_list = serializers.ListField(child=serializers.IntegerField(), required=False)
+    story_location_id_list = serializers.ListField(child=serializers.IntegerField(), required=False)
+    story_tag_id_list = serializers.ListField(child=serializers.IntegerField(), required=False)
+
+    def get_id_lists(self, validated_data):
+        return {
+            "profile_id_list": validated_data.get("profile_id_list", []),
+            "story_location_id_list": validated_data.get("story_location_id_list", []),
+            "story_tag_id_list": validated_data.get("story_tag_id_list", []),
+        }
 
     def follow_with_profile(self, profile, validated_data):
-        profile_uuid_list = validated_data["profile_uuid_list"]
-        story_location_uuid_list = validated_data["story_location_uuid_list"]
+        id_lists = self.get_id_lists(validated_data)
+        profile_id_list = id_lists["profile_id_list"]
+        story_location_id_list = id_lists["story_location_id_list"]
+        story_tag_id_list = id_lists["story_tag_id_list"]
 
-        if profile_uuid_list:
-            profiles_to_follow = Profile.objects.filter(uuid__in=profile_uuid_list)
+        if profile_id_list:
+            profiles_to_follow = Profile.objects.filter(id__in=profile_id_list)
             profile.followed_authors.add(*profiles_to_follow)
 
-        if story_location_uuid_list:
+        if story_location_id_list:
             locations_to_follow = StoryLocations.objects.filter(
-                uuid__in=story_location_uuid_list
+                id__in=story_location_id_list,
             )
             profile.followed_locations.add(*locations_to_follow)
 
+        if story_tag_id_list:
+            tags_to_follow = StoryTags.objects.filter(id__in=story_tag_id_list)
+            profile.followed_tags.add(*tags_to_follow)
+
     def unfollow_with_profile(self, profile, validated_data):
-        profile_uuid_list = validated_data["profile_uuid_list"]
-        story_location_uuid_list = validated_data["story_location_uuid_list"]
+        id_lists = self.get_id_lists(validated_data)
+        profile_id_list = id_lists["profile_id_list"]
+        story_location_id_list = id_lists["story_location_id_list"]
+        story_tag_id_list = id_lists["story_tag_id_list"]
 
-        if profile_uuid_list:
-            profiles_to_follow = Profile.objects.filter(uuid__in=profile_uuid_list)
-            profile.followed_authors.remove(*profiles_to_follow)
+        if profile_id_list:
+            profiles_to_unfollow = Profile.objects.filter(id__in=profile_id_list)
+            profile.followed_authors.remove(*profiles_to_unfollow)
 
-        if story_location_uuid_list:
-            locations_to_follow = StoryLocations.objects.filter(
-                uuid__in=story_location_uuid_list
+        if story_location_id_list:
+            locations_to_unfollow = StoryLocations.objects.filter(
+                id__in=story_location_id_list
             )
-            profile.followed_locations.remove(*locations_to_follow)
+            profile.followed_locations.remove(*locations_to_unfollow)
+
+        if story_tag_id_list:
+            tags_to_unfollow = StoryTags.objects.filter(
+                id__in=story_tag_id_list,
+            )
+            profile.followed_tags.remove(*tags_to_unfollow)
