f08c5a6708ef12b66233730753b3da11e00d2ad1
berkkarahan <berkkarahan00@gmail.com>
2021-06-30 22:01:50 +0300

    sentry integrations

--- coreapp/coreapp/settings.py
+++ coreapp/coreapp/settings.py
@@ -256,9 +256,14 @@ SENTRY_DSN = os.environ.get("SENTRY_DSN")
 if SENTRY_DSN:
     import sentry_sdk
     from sentry_sdk.integrations.django import DjangoIntegration
+    from sentry_sdk.integrations.celery import CeleryIntegration
 
     sentry_sdk.init(
         dsn=SENTRY_DSN,
         traces_sample_rate=1.0,
         send_default_pii=True,
+        integrations=[
+            DjangoIntegration(),
+            CeleryIntegration(),
+        ]
     )
